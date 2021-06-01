76f6e8264f15ff16e926cc0f787606710b768b4b
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-01 13:26:25 +0000

    add basic read functionality to account profile table

--- app.py
+++ app.py
@@ -84,6 +84,12 @@ def sign_up():
     return render_template("sign_up.html")
 
 
+@app.route("/account_profile")
+def get_account_profile():
+    accounts = list(mongo.db.accounts.find())
+    return render_template("account.html", accounts=accounts)
+
+
 @app.route("/account<username>", methods=["GET", "POST"])
 def account(username):
     # grab the session user's username from db
@@ -91,7 +97,7 @@ def account(username):
         {"username": session["user"]})["username"]
 
     if session['user']:
-        return render_template("account.html", username=username)
+        return redirect(url_for("get_account_profile"))
 
     return redirect(url_for("log_in"))
 
