2df73ac445d44eaf6fd6af0c40fc491f1cc50023
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-01 10:12:35 +0000

    add basic table structure to account.html

--- static/js/init.js
+++ static/js/init.js
@@ -7,3 +7,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.chips');
+  var instances = M.Chips.init(elems, options);
+});
