57fc97f7fc8cb8125459f1001fcfb4d45b48d9e4
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-01 08:07:29 +0000

    add basic chart structure using Google charts

--- static/js/init.js
+++ static/js/init.js
@@ -6,3 +6,61 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// Google charts - code taken from https://developers.google.com/chart/interactive/docs/quick_start and modified
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawPieChart);
+google.charts.setOnLoadCallback(drawBarChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawPieChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
+  ]);
+
+  // Set chart options
+  var options = {'title':'How Much Pizza I Ate Last Night',
+                 'width':400,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('pie_chart_div'));
+  chart.draw(data, options);
+}
+
+function drawBarChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
+  ]);
+
+  // Set chart options
+  var options = {'title':'How Much Pizza I Ate Last Night',
+                 'width':400,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.BarChart(document.getElementById('bar_chart_div'));  
+  chart.draw(data, options);
+}
