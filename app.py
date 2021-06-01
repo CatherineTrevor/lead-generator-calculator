0a7cbc2d786b10f2b9c5164dac5aaa24097ff308
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-01 11:49:13 +0000

    add basic log in functionality

--- app.py
+++ app.py
@@ -34,8 +34,32 @@ def benchmark_data():
     return render_template("benchmark_data.html")
 
 
-@app.route("/log_in")
+@app.route("/log_in", methods=["GET", "POST"])
 def log_in():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "account", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("log_in"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("log_in"))
+
     return render_template("log_in.html")
 
 
