f915787e7c5805e9192a52b7446ccc5dfbc526a9
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-01 07:18:42 +0000

    move index.html content from base.html to new file

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 @app.route("/about")
