c9deccebfe3376bfb209140173c044e4f64a8271
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-29 11:54:57 +0000

    update debug=True to False

--- app.py
+++ app.py
@@ -608,4 +608,4 @@ def contact_us():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
